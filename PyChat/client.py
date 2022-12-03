8b065ed94120cb708a10e6be0faa08f58e07945f
Alex K <alexkatrenkos@gmail.com>
2022-12-03 10:47:24 +0200

    v0.2

--- PyChat/client.py
+++ PyChat/client.py
@@ -1,24 +1,24 @@
 import socket
 import select
-import sys
 from config import HOST, PORT
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((HOST, PORT))
 
-while True:
-    sockets_list = [sys.stdin, server]
+class Client():
+    def __init__(slef):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((HOST, PORT))
 
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    def send_message(self, message)
+        # while True:
+        #     sockets_list = [sys.stdin, server]
 
-    for socks in read_sockets:
-        if socks == server:
-            message = socks.recv(2048)
-            print(message.decode())
-        else:
-            message = sys.stdin.readline()
-            server.send(message.encode())
-            sys.stdout.write('[You]')
-            sys.stdout.write(message)
-            sys.stdout.flush()
-server.close()
+        #     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+        #     for socks in read_sockets:
+        #         if socks == server:
+        #             message = socks.recv(2048)
+        #         else:
+        self.server.send(message.encode())
+
+    def close_connection():
+        self.server.close()
