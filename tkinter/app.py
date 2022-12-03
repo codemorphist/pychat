424b9d81df3c6925cabcc215279c38907fe494fc
Alex K <alexkatrenkos@gmail.com>
2022-12-03 11:23:58 +0200

    v0.2

--- tkinter/app.py
+++ tkinter/app.py
@@ -1,50 +1,72 @@
 from tkinter import *
+from client import Client
+
+import sys
+import select
+
+from threading import *
 
 
 class PyChatApp():
 
     def __init__(self):
-        self.message = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                          ``Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'''
+        self.client = Client()
 
+        self.message = 'hello world!\n'
         self.app = Tk()
         self.app.title('PYCHAT')
-        self.app.geometry('400x500')
+        self.app.geometry('400x600')
         self.app.resizable(False, False)
         self.app.config(bg='#FFF')
 
         self.label_text = StringVar()
         self.label_text.set('Hello World!')
 
-        self.label = Label(textvariable=self.label_text, width=40, heigh=10, anchor=W, justify=RIGHT)
-        self.label.pack(expand=True)
+        # self.label = Label(textvariable=self.label_text, width=50, heigh=10, anchor=W, justify=RIGHT)
+        # self.label.pack(expand=True)
 
-        self.text_box = Text(self.app, width=40, heigh=10, wrap='word')
-        self.text_box.pack(expand=True)
+        self.message_box = Text(self.app, width=400, heigh=20, wrap='word')
+        self.message_box.pack(ipadx=0, ipady=0)
+        self.message_box.insert('end', self.message)
+
+
+        self.text_box = Text(self.app, width=400, heigh=10, wrap='word')
+        self.text_box.pack(ipadx=0, ipady=0)
         self.text_box.insert('end', self.message)
 
-        self.clear_button = Button(self.app, text='Clear', width=15, heigh=2, command=self.clear_text_box)
-        self.clear_button.pack(expand=True)
+        self.print_button = Button(self.app, text='Print', width=15, heigh=2, command=self.print_message)
+        self.print_button.pack(ipadx=0, ipady=0, fill=BOTH, expand=True, side=LEFT)
 
-        self.print_button = Button(self.app, text='Print', width=15, heigh=2, command=self.print_text_label)
-        self.print_button.pack(expand=True)
+        self.sync_button = Button(self.app, text='Sync', width=15, heigh=2, command=self.go_ahead())
+        self.sync_button.pack(ipadx=0, ipady=0, fill=BOTH, expand=True, side=RIGHT)
 
+        self.app.mainloop()
 
-    def run(self):
-        try:
-            self.app.mainloop()
-        except Exception as e:
-            print(e)
-
+    def go_ahead(self):
+        rcv = Thread(target=self.recive_message)
+        rcv.start()
 
     def clear_text_box(self):
         self.text_box.delete(1.0, 'end')
-        self.label_text.set('')
 
+    def print_message(self):
+        mes = self.text_box.get(1.0, 'end')
+        self.message_box.insert('end', '[YOU] ' + mes)
+        self.text_box.delete(1.0, 'end')
 
-    def print_text_label(self):
-        self.label_text.set(self.text_box.get(1.0,'end'))
+        self.client.send_message(mes)
 
-app = PyChatApp().run()
+    def recive_message(self):
+        while True:
+            sockets_list = [sys.stdin, self.client.server]
+
+            read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+            for socks in read_sockets:
+                if socks == self.client.server:
+                     message = socks.recv(2048)
+                     self.message_box.insert('end', message)
+                     print(message)
+
+app = PyChatApp()
+app.run()
---
+++ tkinter/client.py
@@ -0,0 +1,23 @@
+import socket
+from config import HOST, PORT
+
+
+class Client():
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((HOST, PORT))
+
+    def send_message(self, message):
+        # while True:
+        #     sockets_list = [sys.stdin, server]
+
+        #     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+        #     for socks in read_sockets:
+        #         if socks == server:
+        #             message = socks.recv(2048)
+        #         else:
+        self.server.send(message.encode())
+
+    def close_connection():
+        self.server.close()
